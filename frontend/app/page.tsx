6247e87b0b9b92f47659a0eb078aa27da200fe94
mrxxx000 <akmalsafi43@gmail.com>
2025-12-11 21:56:09 +0100

    fix

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -782,7 +782,6 @@ export default function Home() {
       <HeroSection />
       <LatestScams />
       <MostSearchedNumbers />
-      <PopularScamNumbers />
       <ScamTypesSection />
       <WarningsSection />
       <Footer />
